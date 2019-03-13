839bb8f74382e2a5de18aa24be55c531462f8680
Theo Azriel <theoazrie@gmail.com>
2019-03-13 15:41:40 +0800

    First commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Main from './components/MainComponent';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Main/>
+    );
+  }
+}
+
+
