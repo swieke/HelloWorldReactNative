cdb37986c841ee421d30ae9390da7114ba2e6733
Theo Azriel <theoazrie@gmail.com>
2019-03-15 22:36:20 +0800

    Added Navigation

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -5,27 +5,30 @@ import Dishdetail from './DishdetailComponent';
 import {View , Platform} from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
-const MenuNavigator = createStackNavigator({
+const MenuNavigator = createStackNavigator(
+
+    {
     Menu: { screen: Menu },
     Dishdetail: { screen: Dishdetail }
-},
-{
-    initialRouteName: 'Menu',
-    navigationOptions: {
-        headerStyle: {
-            backgroundColor: "#512DA8"
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-            color: "#fff"            
+    },
+
+    {
+        initialRouteName: 'Menu',
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: "#512DA8"
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: "#fff"            
+            }
         }
     }
-}
 
 
 );
 
-const Navid = createAppContainer(MenuNavigator);
+const Navid = createAppContainer(MenuNavigator); //Navid itu Navigator
 
 class Main extends React.Component {
     
@@ -33,7 +36,7 @@ class Main extends React.Component {
     render() {
         return(
             <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
-                <Navid />
+                <Navid /> 
             </View>
 
         );
